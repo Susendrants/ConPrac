8d3d8c0345cd65ad4c71b377049fb20d79489c0e
naveen <naveenksb1198@gmail.com>
2024-02-15 10:31:58 +0530

    Merge branch 'master' of https://github.com/Susendrants/ConPrac.git

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -5,7 +5,7 @@ public class TestCase1 {
 	
 	System.out.println("second edit by susee");
 		System.out.println("naveen 1st commit");
-	   
+	   System.out.println("3rd edit by nk");
 		System.out.println("naveen 2nd commit");
 	
   }
